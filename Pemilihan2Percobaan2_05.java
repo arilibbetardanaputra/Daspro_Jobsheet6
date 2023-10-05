0b32262f5772469aa0966201fd0c4cd719bafe48
arilibbetardanaputra <arilardana111@gmail.com>
2023-10-05 19:13:34 +0700

    :fire: Ini adalah project percobaan 2

--- Pemilihan2Percobaan2_05.java
+++ Pemilihan2Percobaan2_05.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan2_05 {
     public static void main(String[] args) {
         Scanner input05 = new Scanner(System.in);
-        int totalSudut, sudut1, sudut2, sudut3;
+        int sudut1, sudut2, sudut3;
 
         System.out.println("Masukkan Sudut 1 : ");
         sudut1 = input05.nextInt();
@@ -12,17 +12,16 @@ public class Pemilihan2Percobaan2_05 {
         System.out.println("Masukkan Sudut 3 : ");
         sudut3 = input05.nextInt();
 
-        totalSudut = sudut1 + sudut2 + sudut3;
-
-        if (totalSudut == 180) {
-            if (sudut1 == 90  sudut2 == 90  sudut3 == 90)
+        if (sudut1 + sudut2 + sudut3 == 180) {
+            if (sudut1 == 90 || sudut2 == 90 || sudut3 == 90) {
                 System.out.println("Segitiga Tersebut Adalah Segitiga Siku-siku");
-            else if (sudut1 == sudut2 && sudut2 == sudut3)
+            } else if (sudut1 == sudut2 && sudut2 == sudut3) {
                 System.out.println("Segitiga Tersebut Adalah Segitiga Sama Sisi");
-            else if (sudut1 == sudut2  sudut2 == sudut3  sudut1 == sudut3)
+            } else if (sudut1 == sudut2 || sudut2 == sudut3 || sudut1 == sudut3) {
                 System.out.println("Segitiga Tersebut Adalah Segitiga Sama Kaki");
-            else
+            } else {
                 System.out.println("Segitiga Tersebut Adalah Segitiga Sembarang");
+            }
         } else {
             System.out.println("Bukan Segitiga");
         }
