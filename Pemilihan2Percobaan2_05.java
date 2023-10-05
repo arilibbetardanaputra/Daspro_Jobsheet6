c5c704d253edf869190ce55a392fceb6cfbd749f
arilibbetardanaputra <arilardana111@gmail.com>
2023-10-05 19:08:49 +0700

    :fire: Ini adalah project percobaan 2

--- Pemilihan2Percobaan2_05.java
+++ Pemilihan2Percobaan2_05.java
@@ -15,11 +15,11 @@ public class Pemilihan2Percobaan2_05 {
         totalSudut = sudut1 + sudut2 + sudut3;
 
         if (totalSudut == 180) {
-            if (sudut1 == 90 || sudut2 == 90 || sudut3 == 90)
+            if (sudut1 == 90  sudut2 == 90  sudut3 == 90)
                 System.out.println("Segitiga Tersebut Adalah Segitiga Siku-siku");
             else if (sudut1 == sudut2 && sudut2 == sudut3)
                 System.out.println("Segitiga Tersebut Adalah Segitiga Sama Sisi");
-            else if (sudut1 == sudut2 || sudut2 == sudut3 || sudut1 == sudut3)
+            else if (sudut1 == sudut2  sudut2 == sudut3  sudut1 == sudut3)
                 System.out.println("Segitiga Tersebut Adalah Segitiga Sama Kaki");
             else
                 System.out.println("Segitiga Tersebut Adalah Segitiga Sembarang");
